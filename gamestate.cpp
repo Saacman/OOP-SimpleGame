f5d77eb67e38a42cc11704fc62c2c46c1cc202f5
Isaac <saacman27@gmail.com>
2018-12-03 12:13:31 -0600

    Cambio de lógica de colisiones

--- gamestate.cpp
+++ gamestate.cpp
@@ -1,35 +1,63 @@
 #include "gamestate.hpp"
+#include <vector>
+#include <iostream>
 
-GameState::GameState(ResourcePath& Path) : path(Path){ }
+GameState::GameState(ResourcePath& Path, FSM& fsm) : path(Path), fsm(fsm), switchToState(0) { }
 
 
 void GameState::init() {
     playerTexture.loadFromFile(path.Get() + "player.png");
     player = Player(&playerTexture, sf::Vector2u(4, 4), 0.1f, 100.0f);
+    enemy1 = Enemy(nullptr, sf::Vector2u(4, 4), 0.1f, 100.0f);
 
     backTexture.loadFromFile(path.Get() + "background.png");
     backSprite.setTexture(backTexture);
-    house1 = Box(sf::Vector2f(98.0f, 102.0f), sf::Vector2f(51.0f, 295.0f));
-    house2 = Box(sf::Vector2f(98.0f, 102.0f), sf::Vector2f(182.0f, 295.0f));
-    house3 = Box(sf::Vector2f(98.0f, 102.0f), sf::Vector2f(313.0f, 295.0f));
-    house4 = Box(sf::Vector2f(227.0f, 102.0f), sf::Vector2f(116.5f, 57.0f));
+    // Definiendo el vector de paredes
+    walls.push_back(Box(sf::Vector2f(98.0f, 102.0f), sf::Vector2f(51.0f, 295.0f)));
+    walls.push_back(Box(sf::Vector2f(98.0f, 102.0f), sf::Vector2f(182.0f, 295.0f)));
+    walls.push_back(Box(sf::Vector2f(98.0f, 102.0f), sf::Vector2f(313.0f, 295.0f)));
+    walls.push_back(Box(sf::Vector2f(227.0f, 102.0f), sf::Vector2f(116.5f, 57.0f)));
 }
 
 void GameState::terminate() { }
 
+void GameState::SetSwitchToState(unsigned int id) {
+	// Almacena el id del estado al que pasará al terminar
+    switchToState = id;
+}
 
 void GameState::Update(float dTime) {
+    enemy1.Update(dTime);
+    if (player.GetPosition().x < 193.0f && player.GetPosition().x > 172.0f) {
+        if (player.GetPosition().y < 345.0f && player.GetPosition().y > 316.0f) {
+            fsm.SwitchTo(switchToState);
+        }
+    }
+    if (player.GetPosition().x < 62.0f && player.GetPosition().x > 41.0f) {
+        if (player.GetPosition().y < 345.0f && player.GetPosition().y > 316.0f) {
+            fsm.SwitchTo(switchToState);
+        }
+    }
+    if (player.GetPosition().x < 323.0f && player.GetPosition().x > 303.0f) {
+        if (player.GetPosition().y < 345.0f && player.GetPosition().y > 316.0f) {
+            fsm.SwitchTo(switchToState);
+        }
+    }
+    player.CanMove();
+    for( auto item = walls.begin(); item != walls.end(); item++ ) {
+        if(player.GetBody().getGlobalBounds().intersects(item->GetBody().getGlobalBounds())) {
+            for(unsigned int i = 0; i <= 3; i++) {
+                if (player.GetDirection() == i)  player.CantMove(i);
+            }
+        }
+    }
 
     player.Update(dTime);
-    // platform1.GetCollider().CheckCollider(player.GetCollider(),1.0f);
-    // platform2.GetCollider().CheckCollider(player.GetCollider(),1.0f); // Valores entre 0 y 1
 }
 void GameState::Draw(Window& window) {
     window.Draw(backSprite);
-    house1.Draw(window);
-    house2.Draw(window);
-    house3.Draw(window);
-    house4.Draw(window);
+    for( auto item = walls.begin(); item != walls.end(); item++ ) item->Draw(window);
     player.Draw(window);
+    enemy1.Draw(window);
 
 }
--- gamestate.hpp
+++ gamestate.hpp
@@ -9,14 +9,17 @@
 #include "player.hpp"
 #include "animation.hpp"
 //#include "collider.hpp"
+#include "enemy.hpp"
+#include "fsm.hpp"
 class GameState : public State {
 public:
-    GameState(ResourcePath& Path);
+    GameState(ResourcePath& Path, FSM& fsm);
 
     void init() override;
     void terminate() override;
     void Update(float dTime) override;
     void Draw(Window& window) override;
+    void SetSwitchToState(unsigned int id);
 
 private:
     Player player;
@@ -24,10 +27,14 @@ private:
     sf::Sprite playerSprite;
     sf::Texture backTexture;
     sf::Sprite backSprite;
-    Box house1;
-    Box house2;
-    Box house3;
-    Box house4;
+    std::vector<Box> walls;
+    // Box house1;
+    // Box house2;
+    // Box house3;
+    // Box house4;
     ResourcePath& path;
+    Enemy enemy1;
+    FSM& fsm;
+    unsigned int switchToState;
 };
 #endif
